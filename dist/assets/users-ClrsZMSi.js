8b147108671c01b034298d6f5be5fcc305372901
aashikantkumar <aashikant.kumar2023@iem.edu.in>
2025-08-03 23:52:12 +0530

    feat: update assets and components for video production section
    - Removed outdated social media assets and added new ones.
    - Introduced new JavaScript files for icons (star, users, video, zap).
    - Updated index.html to reference the latest JavaScript and CSS files.
    - Added new image asset for portfolio section.
    - Modified PortfolioSection component to include the new image.
    - Updated logo.png with a new version.

---
+++ dist/assets/users-ClrsZMSi.js
@@ -0,0 +1,16 @@
+import{c}from"./index-Bc7wtA6R.js";/**
+ * @license lucide-react v0.522.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M3 3v16a2 2 0 0 0 2 2h16",key:"c24i48"}],["path",{d:"M18 17V9",key:"2bz60n"}],["path",{d:"M13 17V5",key:"1frdt8"}],["path",{d:"M8 17v-3",key:"17ska0"}]],s=c("chart-column",a);/**
+ * @license lucide-react v0.522.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z",key:"c3ymky"}]],d=c("heart",t);/**
+ * @license lucide-react v0.522.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["path",{d:"M16 3.128a4 4 0 0 1 0 7.744",key:"16gr8j"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}]],n=c("users",e);export{s as C,d as H,n as U};
